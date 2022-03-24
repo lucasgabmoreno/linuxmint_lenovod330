7abb8a15546a947be2d54f50c94e66d46994ab44
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-03-23 21:51:33 -0300

    Delete d330fix.sh

--- d330fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#RESET DEFAULT
-xrandr -o normal
-
-#FIX
-xrandr -o right
-TOUCHSCREEN_ID=$(xinput list --id-only "pointer:Goodix Capacitive TouchScreen")
-xinput set-prop "$TOUCHSCREEN_ID" --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
