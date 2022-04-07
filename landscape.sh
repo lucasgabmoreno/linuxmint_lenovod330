158d17b0484357c3c934031ed0d2a580ddca9df6
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-04-06 21:21:04 -0300

    Add files via upload

--- landscape.sh
+++ landscape.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 INDEV=$(xinput list --id-only "pointer:Goodix Capacitive TouchScreen") # Touchscreen
-DNAME=$(xrandr --listmonitors | sed -ne 's/ .* //gp') # Monitor
-MODE=$(echo $(xrandr | grep '*') | awk -F " " '{print $1; exit}') # Resolution 
-RATE=$(echo $(xrandr | grep '*') | awk -F " " '{print $2; exit}' | sed 's/\*+//') # Rate Hz
 BRIGHT=$(echo $(xrandr --verbose | grep 'Brightness') | awk -F " " '{print $2; exit}') # Brightness
 GAMMA=$(echo $(xrandr --verbose | grep 'Gamma') | awk -F " " '{print $2; exit}') # Gamma
+#DNAME=$(xrandr --listmonitors | sed -ne 's/ .* //gp') # Monitor
+#MODE=$(echo $(xrandr | grep '*') | awk -F " " '{print $1; exit}') # Resolution 
+#RATE=$(echo $(xrandr | grep '*') | awk -F " " '{print $2; exit}' | sed 's/\*+//') # Rate Hz
+DNAME=DSI-1 # Monitor
+MODE_PRE=1024x768 # Pre resolution
+MODE=800x1280 # Resolution 
+RATE_PRE=60.04 # Pre Rate Hz
+RATE=60.00 # Rate Hz
 
-xrandr --output $DNAME --mode $MODE --rate $RATE --gamma $GAMMA --brightness $BRIGHT --primary --rotate normal  
+xrandr --output $DNAME --mode $MODE_PRE --rate $RATE_PRE --gamma $GAMMA --brightness $BRIGHT --primary --rotate right
 xrandr --output $DNAME --mode $MODE --rate $RATE --gamma $GAMMA --brightness $BRIGHT --primary --rotate right 
 xinput set-prop "$INDEV" --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+
