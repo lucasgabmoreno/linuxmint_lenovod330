5c9e35cdbe48fe4cc71169ec3c3f9dff39491be4
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-04-22 19:23:36 -0300

    Add files via upload

--- lenovod330-webcam.sh
+++ lenovod330-webcam.sh
@@ -3,14 +3,5 @@
 # Make default front camera
 
 # Fix
-sudo mv /dev/video0 /dev/video0.bak
-sudo mv /dev/video1 /dev/video1.bak
-sudo mv /dev/video2 /dev/video0
-sudo mv /dev/video3 /dev/video1
-sudo mv /dev/video0.bak /dev/video2
-sudo mv /dev/video1.bak /dev/video3
-
-# Remove
-sudo rm -rf /etc/systemd/system/lenovod330-webcam.service
-sudo rm -rf /usr/bin/lenovod330-webcam.sh
-
+sudo mv /dev/video0 /dev/video4
+sudo mv /dev/video1 /dev/video5
