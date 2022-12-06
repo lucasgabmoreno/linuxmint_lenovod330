b900c676644cdd95b4cd908a462078faeccfab72
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-12-05 21:23:06 -0300

    rotate camera instead  of delete

--- lenovod330-10igl-webcam.sh
+++ lenovod330-10igl-webcam.sh
@@ -3,6 +3,9 @@
 # Fix Lenovo Ideapad D330-10IGL default camera
 # Created for Linux Mint Cinnamon
 
+# Dependencies
+# sudo apt install v4l-utils -y
+
 # Copy this file into /usr/bin/ folder
 # Copy lenovod330-10igl-webcam.service into /etc/systemd/system/ folder
 # sudo chmod +x /usr/bin/lenovod330-10igl-webcam.sh
@@ -11,7 +14,12 @@
 # sudo systemctl start lenovod330-10igl-webcam.service
 # reboot
 
-# FRONT 2M CAMERA DEFAULT
-# Remove back 5M camera
-sudo rm -rf /dev/video0
-sudo rm -rf /dev/video1
+if [[ $(v4l2-ctl --info --device /dev/video0 | grep Model) == *5M* ]]
+then
+sudo mv /dev/video0 /dev/video10
+sudo mv /dev/video1 /dev/video11
+sudo mv /dev/video2 /dev/video0
+sudo mv /dev/video3 /dev/video1
+sudo mv /dev/video10 /dev/video2
+sudo mv /dev/video11 /dev/video3
+fi
